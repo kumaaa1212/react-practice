47e95779a1f0f5ccce952a324f6262f3b7f5fca1
Ryohei <harrry.kuma1225@gmail.com>
2023-11-26 19:35:17 +0900

    add : variables

--- src/components/variables/index.tsx
+++ src/components/variables/index.tsx
@@ -10,6 +10,16 @@ export default function Variables() {
   };
   obj.a = "bbbb";
   console.log(obj);
+
+  const handleData = (data: string): void => {
+    data = "bbbb";
+    // ここでは、dataは"bbbb"になる
+
+    // この前にconstかletをつけるとエラーになる。
+    // これは関数の引数として暗黙的に宣言されているから、再度宣言することはできない。(再代入はletでできる)
+  };
+  handleData("aaaa");
+
   return (
     <div>
       <h3>Variables</h3>
