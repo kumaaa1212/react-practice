08d09b6fabc824702f49e3206ca39c86c9e64580
Ryohei <harrry.kuma1225@gmail.com>
2023-11-27 21:13:25 +0900

    fix format

---
+++ src/components/JavaScript/index.tsx
@@ -0,0 +1,14 @@
+import DataType from "./dataType";
+import PrerequisiteKnowledge from "./prerequisiteKnowledge";
+import Variables from "./variables";
+
+export default function JavaScript() {
+  return (
+    <div>
+      <h2>JavaScript</h2>
+      <PrerequisiteKnowledge />
+      <Variables />
+      <DataType />
+    </div>
+  );
+}
