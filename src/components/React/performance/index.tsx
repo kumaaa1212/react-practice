0bf3ab837c35353d206ba4dd80a9398e19fed568
Ryohei <harrry.kuma1225@gmail.com>
2023-12-05 18:30:40 +0900

    add hooks-use

--- src/components/React/performance/index.tsx
+++ src/components/React/performance/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 type Obj = {
   a: number;
@@ -9,29 +9,56 @@ export default function Performance() {
     a: 1,
   };
   // トリガー
-   // レンダリングが予約されるタイミング
+  // レンダリングが予約されるタイミング
+
   // レンダリング
-   //・初回レンダリング
+  //・初回レンダリング
+
   //・stateが更新されたとき
-   // コミット
+  // コミット
 
-
-// この下の例だとレンダリングが起きているが、参照しているものが同じなので、レンダリングが起きていない。
+  // この下の例だとレンダリングが起きているが、参照しているものが同じなので、レンダリングが起きていない。
+  // だから他のところでレンダリングが起きたタイミングで表示が一気に変わる
   const [state, setState] = useState<Obj>(obj);
   const handle = () => {
     setState((prev) => {
-      prev.a ++;
+      prev.a++;
       console.log(prev);
       return prev;
     });
   };
 
+  // useCallback
+
+  const [count, setCount] = useState(0);
+  const handleClick = useCallback(() => {
+    setCount((prev) => prev + 1);
+  }, []);
+  // const handleClick = useCallback(() => {
+  //   setCount(count + 1);
+  // }, [count]);
+  // prevという文字が変わっていないから大丈夫。
+  // countを入れてしまうと、更新が必要になる
+
+
+  // usememo
+  // メモ化→キャッシュ化→値を再計算せずに、値をどこかに保存してそれを使う。
+  const [number, setNumber] = useState(0);
+  const double = (number: number) => {
+    let i = 0;
+    i = number * 2;
+    return i
+  };
+
+  const doubledCount = useMemo(() => double(number), [number]);
 
   return (
     <div>
       <h1>Performance</h1>
       <button onClick={handle}>レンダリングの確認</button>
       <p>{state.a}</p>
+      <button onClick={handleClick}>useCallbackの確認</button>
+      <p>{count}</p>
     </div>
   );
 }
--- src/components/React/performance/performance1.tsx
+++ src/components/React/performance/performance1.tsx
@@ -1,14 +1,15 @@
 import { memo } from "react";
+// カスタム比較関数
+// 渡ってきたpropsが同じならtrueを返す。
+// 渡ってきたpropsが違うならfalseを返す。
+// 依存関係に入れて、判断している
 
-const Performance1 = memo(() => {
-  return (
-    <div>
-      <h3>Performance1</h3>
-    </div>
-  );
-});
-export default Performance1;
-
+function component(oldProps: any, newProps: any): boolean {
+  if (oldProps === newProps) {
+    return true;
+  }
+  return false;
+}
 
 export const Performance2 = memo(() => {
   return (
@@ -16,4 +17,4 @@ export const Performance2 = memo(() => {
       <h3>Performance1</h3>
     </div>
   );
-});
+}, component);
