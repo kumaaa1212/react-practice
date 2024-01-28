82cc69efd776e57d330e1beea1d44234f8d99998
Ryohei <harrry.kuma1225@gmail.com>
2024-01-28 16:03:53 +0900

    fix line

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import JavaScript from "./components/JavaScript";
 import React from "./components/React";
 import TodoApp from "./deliverables/todo-app-1";
 function App() {
   const [state, setState] = useState<boolean>();
+
+  setTimeout(() => {
+    console.log("Hello");
+  }, 1000);
+  new Promise<void>((resolve, reject) => {
+    console.log("SSS");
+    resolve();
+  }).then(() => {
+    console.log("SSS");
+  });
+
   return (
     <div className="App">
       <button className="display_btn" onClick={() => setState(!state)}>
@@ -16,3 +27,5 @@ function App() {
   );
 }
 export default App;
+
+
---
+++ src/components/JavaScript/asynchronousProcessing/index.tsx
@@ -0,0 +1,6 @@
+
+export default function AsynchronousProcessing() {
+  return (
+    <div></div>
+  )
+}
